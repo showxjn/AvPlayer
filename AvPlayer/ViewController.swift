899e279a01ef4d15fb01f1b8152365bf2b6a15b2
showxjn <luojian9568@sina.com>
2017-04-01 08:59:41 +0800

    new

--- AvPlayer/ViewController.swift
+++ AvPlayer/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+import AVKit
+import AVFoundation
+
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		let session = AVAudioSession.sharedInstance()
+        do {
+			try session.setCategory(AVAudioSessionCategoryPlayback)
+		} catch is Error {
+			print("error")
+		}
+		
+		let _player = AVPlayer(playerItem: AVPlayerItem(url: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!))
+		let playervc = AVPlayerViewController()
+		playervc.player = _player
+		playervc.showsPlaybackControls = true
+		
+		self.addChildViewController(playervc)
+		playervc.view.frame = CGRect(x: 0, y: 0, width: 400, height: 300)
+		view.addSubview(playervc.view)
+		playervc.player?.play()
+		
 	}
 
 	override func didReceiveMemoryWarning() {
